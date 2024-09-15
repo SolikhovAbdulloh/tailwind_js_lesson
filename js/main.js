76a833099b5d192497b870839baacb1cb868c033
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-15 14:22:29 +0500

    finish header

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// arrayReturn ([1,2,2,3,4,5,5]) array ichida
+//  qaytailgan valularni olib tashlab yangi array qaytarish
+//  . (Hecha qanday Set() va arrayning include metodini ishlatish mumkin emas)
+//   2) sortArray ([34,23,45,62]) funskiya chaqrilgan unga berilgan array 
+// ichidagi value kottasidan kichigiga qarab joylashgan array qaytarsin Output:[23,34,45,62]
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/html*/*.{html,js}"],
+  content: ["./html/*.{html,js}"],
   theme: {
     extend: {},
   },
