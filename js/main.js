01e5cb54f8948b45363d5e42e85c6a6215a72537
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-16 08:12:18 +0500

    m

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@
 //   2) sortArray ([34,23,45,62]) funskiya chaqrilgan unga berilgan array 
 // ichidagi value kottasidan kichigiga qarab joylashgan array qaytarsin Output:[23,34,45,62]
 
+
+
+
+
+
+// let arr = [1,2,2,3,4,5,5]
+
+
+// arrayReturn(arr)
+
+
+// function arrayReturn(arr){
+//     let newArr = []
+
+//     for(let i = 0 ; i < arr.length ; i++){
+//        if(arr[i] !== arr[i+1]){
+//             newArr += arr[i]
+//        }
+//     }
+//     console.log(newArr);
+    
+// }
